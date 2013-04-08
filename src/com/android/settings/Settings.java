dbc03ffaaf6346db63f23547c7239edfe3f7521c
C-RoMMix <mehmettayse@hotmail.com>
2013-04-08 18:48:20 +0200

    Revert "Dark C-RoM"
    This reverts commit c526a92d775c6a1da0204cf5c1a35e0c3becf0f4.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -26,7 +26,6 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.bluetooth.BluetoothEnabler;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.fuelgauge.PowerUsageSummary;
-import com.android.settings.slim.TRDSEnabler;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wifi.WifiEnabler;
 
@@ -98,7 +97,6 @@ public class Settings extends PreferenceActivity
     private Header mCurrentHeader;
     private Header mParentHeader;
     private boolean mInLocalHeaderSwitch;
-    private static Switch mTRDSSwitch;
 
     // Show only these settings for restricted users
     private int[] SETTINGS_FOR_RESTRICTED = {
@@ -126,7 +124,6 @@ public class Settings extends PreferenceActivity
             R.id.accessibility_settings,
             R.id.userinterface,
             R.id.customized,
-            R.id.dcmx_settings,
             R.id.themes,
             R.id.advanced_settings
     };
@@ -611,7 +608,6 @@ public class Settings extends PreferenceActivity
 
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
-        private final TRDSEnabler mTRDSEnabler;
         private AuthenticatorHelper mAuthHelper;
 
         private static class HeaderViewHolder {
@@ -624,11 +620,9 @@ public class Settings extends PreferenceActivity
         private LayoutInflater mInflater;
 
         static int getHeaderType(Header header) {
-            if (header.fragment == null && header.intent == null && header.id != R.id.dcmx_settings) {
+            if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
-            } else if (header.id == R.id.wifi_settings
-                    || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.dcmx_settings) {
+            } else if (header.id == R.id.wifi_settings || header.id == R.id.bluetooth_settings) {
                 return HEADER_TYPE_SWITCH;
             } else {
                 return HEADER_TYPE_NORMAL;
@@ -672,7 +666,6 @@ public class Settings extends PreferenceActivity
             // Switches inflated from their layouts. Must be done before adapter is set in super
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
-            mTRDSEnabler = new TRDSEnabler(context, new Switch(context));
         }
 
         @Override
@@ -731,9 +724,6 @@ public class Settings extends PreferenceActivity
                         mWifiEnabler.setSwitch(holder.switch_);
                     } else {
                         mBluetoothEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.dcmx_settings) {
-                        mTRDSSwitch = (Switch) view.findViewById(R.id.switchWidget);
-                        mTRDSEnabler.setSwitch(holder.switch_);
                     }
                     // No break, fall through on purpose to update common fields
 
@@ -770,13 +760,11 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
-            mTRDSEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
-            mTRDSEnabler.pause();
         }
     }
 
@@ -793,9 +781,6 @@ public class Settings extends PreferenceActivity
             highlightHeader((int) mLastHeader.id);
         } else {
             mLastHeader = header;
-        }
-        if (header.id == R.id.dcmx_settings) {
-            mTRDSSwitch.toggle();
         }
     }
 
