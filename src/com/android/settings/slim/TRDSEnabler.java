aed7058a2cf1c3991557700c238dff497697b0b0
C-RoMMix <mehmettayse@hotmail.com>
2013-04-08 18:47:34 +0200

    Revert "Settings: Add to be sure also some other apps for TRDS toggle"
    This reverts commit 0901318f62c7ec3353027485cefc02e7f8fa23c2.

--- src/com/android/settings/slim/TRDSEnabler.java
+++ src/com/android/settings/slim/TRDSEnabler.java
@@ -66,39 +66,6 @@ public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
         // Handle a switch change
         Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.UI_INVERTED_MODE, isChecked ? 1 : 0);
-<<<<<<< HEAD
-=======
-
-        ActivityManager am = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> pids = am.getRunningAppProcesses();
-           for(int i = 0; i < pids.size(); i++) {
-               ActivityManager.RunningAppProcessInfo info = pids.get(i);
-               if(info.processName.equalsIgnoreCase("com.android.contacts")) {
-                    am.killBackgroundProcesses("com.android.contacts");
-               }
-               if(info.processName.equalsIgnoreCase("com.google.android.gm")) {
-                    am.killBackgroundProcesses("com.google.android.gm");
-               }
-               if(info.processName.equalsIgnoreCase("com.android.email")) {
-                    am.killBackgroundProcesses("com.android.email");
-               }
-               if(info.processName.equalsIgnoreCase("com.android.vending")) {
-                    am.killBackgroundProcesses("com.android.vending");
-               }
-               if(info.processName.equalsIgnoreCase("com.google.android.talk")) {
-                    am.killBackgroundProcesses("com.google.android.talk");
-               }
-               if(info.processName.equalsIgnoreCase("com.android.mms")) {
-                    am.killBackgroundProcesses("com.android.mms");
-               }
-               if(info.processName.equalsIgnoreCase("com.google.android.googlequicksearchbox")) {
-                    am.killBackgroundProcesses("com.google.android.googlequicksearchbox");
-               }
-               if(info.processName.equalsIgnoreCase("com.google.android.youtube")) {
-                    am.killBackgroundProcesses("com.google.android.youtube");
-               }
-           }
->>>>>>> eb8de00... Settings: Add to be sure also some other apps for TRDS toggle
     }
 
 }
